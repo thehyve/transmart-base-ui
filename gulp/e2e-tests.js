aad44caf3bb258d3a7139c40bd1781a8d54ec383
barteld klasens <barteld@thehyve.nl>
2016-12-12 17:05:39 +0100

    add docker selenium

--- gulp/e2e-tests.js
+++ gulp/e2e-tests.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var gulp = require('gulp');
+var shell = require('gulp-shell')
 var conf = require('./conf');
 
 var browserSync = require('browser-sync');
@@ -10,12 +11,13 @@ var $ = require('gulp-load-plugins')();
 
 // Downloads the selenium webdriver
 gulp.task('webdriver-update', $.protractor.webdriver_update);
-
 gulp.task('webdriver-standalone', $.protractor.webdriver_standalone);
 
+gulp.task('docker-start', shell.task(['docker run -d -P -e no_proxy=localhost -e HUB_ENV_no_proxy=localhost -p 0.0.0.0:4444:4444 -p 0.0.0.0:5900:5900 --name gulp-chrome-debug selenium/standalone-chrome-debug']))
+gulp.task('docker-stop', shell.task(['docker stop gulp-chrome-debug', 'docker rm gulp-chrome-debug']))
+
 function runProtractor(done) {
-    var params = process.argv;
-    var args = params.length > 3 ? [params[3], params[4]] : [];
+    var args = process.argv.slice(3);
 
     gulp.src(conf.paths.e2e)
         .pipe($.protractor.protractor({
@@ -33,6 +35,7 @@ function runProtractor(done) {
         });
 }
 
+gulp.task('protractor:verify', runProtractor)
 gulp.task('protractor', ['protractor:src']);
 gulp.task('protractor:src', ['serve:e2e', 'webdriver-update'], runProtractor);
 gulp.task('protractor:dist', ['serve:e2e-dist', 'webdriver-update'], runProtractor);
