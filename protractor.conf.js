5648780c2f0bb0fd2d0ae9b1da543de338162da4
barteld klasens <barteld@thehyve.nl>
2016-12-12 13:51:57 +0100

    configured protractor-cucumber-framework

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,7 +5,7 @@ var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 // An example configuration file.
 exports.config = {
     // The address of a running selenium server.
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
+//    seleniumAddress: 'http://localhost:4444/wd/hub',
     //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
     // Capabilities to be passed to the webdriver instance.
@@ -13,15 +13,25 @@ exports.config = {
         'browserName': 'chrome'
     },
 
-    baseUrl: 'http://localhost:3000',
+    // set to "custom" instead of cucumber.
+    framework: 'custom',
+
+    // path relative to the current config file
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+//    baseUrl: 'http://localhost:3000',
 
     // Spec patterns are relative to the current working directory when
     // protractor is called.
-    specs: [paths.e2e + '/**/*.js'],
+    specs: [paths.e2e + '/features/**/*.feature'],
 
-    // Options to be passed to Jasmine-node.
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000
-    }
+    cucumberOpts: {
+        require: paths.e2e + '/steps/**/*.js'
+    },
+
+//    // Options to be passed to Jasmine-node.
+//    jasmineNodeOpts: {
+//        showColors: true,
+//        defaultTimeoutInterval: 30000
+//    }
 };
